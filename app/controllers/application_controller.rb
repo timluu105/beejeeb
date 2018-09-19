4291e39f3ebbf6c272cea49a8d5c1fdd2c4bbe01
Tim Luu <timluu105@gmail.com>
2018-09-19 21:13:26 +0300

    removed announcement bar from admin pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,13 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
 
-  helper_method :enabled_announcement
+  helper_method :enabled_announcement, :admin_pages?
 
   def enabled_announcement
     @enabled_announcement ||= Announcement.enabled.last
+  end
+
+  def admin_pages?
+    params[:controller].match /\Aadmin/
   end
 end
