e741dedc04abb43c43463d33367c3b8dc56b7b20
Tim Luu <timluu105@gmail.com>
2018-09-20 19:59:38 +0300

    fixes for emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: APP_CONFIG['from_email']
+  default from: "#{APP_CONFIG['title']}<#{APP_CONFIG['from_email']}"
   append_view_path Rails.root.join('app', 'views', 'mailers')
   layout 'mailer'
 end
